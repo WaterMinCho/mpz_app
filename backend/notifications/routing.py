faf8a0f4ef084d650e703758bff1a24853febfb0
HyeyeonJung <jhy20306@gmail.com>
2025-12-09 01:21:52 +0900

    fix: 알림 시스템 수정

--- backend/notifications/routing.py
+++ backend/notifications/routing.py
@@ -4,7 +4,8 @@ from . import consumers
 websocket_urlpatterns = [
     # 사용자별 실시간 알림
     re_path(
-        r"ws/notifications/(?P<user_id>\w+)/$",
+        # UUID 사용자 ID의 하이픈(-)까지 허용하도록 패턴 확장
+        r"ws/notifications/(?P<user_id>[0-9a-fA-F-]+)/$",
         consumers.NotificationConsumer.as_asgi()
     ),
     
